5e9ad71be250e5996dadf8caa2d3a9f2daa95420
koki takahashi <koki.weben.main@gmail.com>
2024-05-17 02:38:14 +0900

    :bug: buildできない問題を解消

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
   description: 'kt-portfolio-site',
 };
 
-export const config: ThemeConfig = {
+export const themeConfig: ThemeConfig = {
   initialColorMode: 'system',
 };
 
@@ -27,7 +27,7 @@ export default function RootLayout({
     <html lang='ja'>
       <body className={inter.className}>
         <ColorModeScript type='cookie' nonce='testing' />
-        <UIProvider config={config} colorModeManager={{ ...colorModeManager }.cookieStorage}>
+        <UIProvider config={themeConfig} colorModeManager={{ ...colorModeManager }.cookieStorage}>
           <Header />
           <Box minHeight={'calc(100vh - 64px)'}>{children}</Box>
           <Footer />
